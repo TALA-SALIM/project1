c8a753629288fd34ceb8a859329fc7c5c8405c65
tala salim <210779523+TALA-SALIM@users.noreply.github.com>
2025-10-20 23:11:42 +0300

    Add files via upload

---
+++ visascrpit.js
@@ -0,0 +1,31 @@
+const nameInput = document.getElementById('input-name');
+const numberInput = document.getElementById('input-number');
+const expiryInput = document.getElementById('input-expiry');
+const cvvInput = document.getElementById('input-cvv');
+
+const cardName = document.getElementById('card-name');
+const cardNumber = document.getElementById('card-number');
+const cardExpiry = document.getElementById('card-expiry');
+const cardCVV = document.getElementById('card-cvv');
+
+const card = document.getElementById('card-inner');
+
+document.getElementById('card').addEventListener('click', () => {
+  card.classList.toggle('flip');
+});
+
+nameInput.addEventListener('input', () => {
+  cardName.textContent = nameInput.value || 'Person Name';
+});
+
+numberInput.addEventListener('input', () => {
+  cardNumber.textContent = numberInput.value || '1111 2222 3333 4444';
+});
+
+expiryInput.addEventListener('input', () => {
+  cardExpiry.textContent = expiryInput.value || '02/30';
+});
+
+cvvInput.addEventListener('input', () => {
+  cardCVV.textContent = cvvInput.value || '***';
+});
