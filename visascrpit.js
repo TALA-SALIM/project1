3312c6900d7cfb514f2bd9e167c5f767d97e410d
tala salim <210779523+TALA-SALIM@users.noreply.github.com>
2026-01-15 01:21:57 +0200

    Update visascrpit.js

--- visascrpit.js
+++ visascrpit.js
@@ -2,16 +2,15 @@ const nameInput = document.getElementById('input-name');
 const numberInput = document.getElementById('input-number');
 const expiryInput = document.getElementById('input-expiry');
 const cvvInput = document.getElementById('input-cvv');
-
 const cardName = document.getElementById('card-name');
 const cardNumber = document.getElementById('card-number');
 const cardExpiry = document.getElementById('card-expiry');
 const cardCVV = document.getElementById('card-cvv');
+const card = document.getElementById('card');
+const cardinner = document.getElementById('card-inner');
 
-const card = document.getElementById('card-inner');
-
-document.getElementById('card').addEventListener('click', () => {
-  card.classList.toggle('flip');
+card.addEventListener('click', () => {
+  cardinner.classList.toggle('flip');
 });
 
 nameInput.addEventListener('input', () => {
@@ -29,3 +28,7 @@ expiryInput.addEventListener('input', () => {
 cvvInput.addEventListener('input', () => {
   cardCVV.textContent = cvvInput.value || '***';
 });
+cvvInput.addEventListener('input', () => {
+  cardCVV.textContent = cvvInput.value || '***';
+});
+
